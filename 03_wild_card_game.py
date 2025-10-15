4cf903533e02158996986e967ccbd6654bdc5534
IStaykova <164316912+IStaykova@users.noreply.github.com>
2025-10-15 20:50:00 +0300

    Added wild_card_game.py

---
+++ 03_wild_card_game.py
@@ -0,0 +1,18 @@
+def draw_cards(*args, **kwargs):
+    monster_cards = []
+    spell_cards = []
+    result = ' '
+    for card_name, card_type in (list(args) + list(kwargs.items())):
+        if card_type == "monster":
+            monster_cards.append(f"  ***{card_name}")
+        elif card_type == "spell":
+            spell_cards.append(f"  $$${card_name}")
+
+    if monster_cards:
+        result = "Monster cards:\n" + "\n".join(sorted(monster_cards, reverse=True)) + "\n"
+    if spell_cards:
+        result += "Spell cards:\n" + "\n".join(sorted(spell_cards))
+
+    return result.strip()
+
+print(draw_cards(("cyber dragon", "monster"), freeze="spell",))
